aacfc082464624c81d316c0b40f12acf5c97b221
Daniel <studiosdopplermusic@gmail.com>
2022-04-19 21:51:49 -0500

    tipado

---
+++ 1_Tipado/1_tipos.ts
@@ -0,0 +1,27 @@
+// Boolean
+    // SI||NO 
+
+let esNull:boolean;
+let esReal:boolean = true; 
+
+
+// Number
+    let numero:number; 
+    
+
+// String
+    let string:string;
+
+
+// Array 
+    let arrayNumeros:number[] = [1,2,3]; 
+    let arrayNumerosDos:Array<number> = [1,2,3];
+    let arrayString:string[] = ["Hola","Mundo"]; 
+    let arrayStringDos:Array<string> = ["Hola","Mundo"]; 
+    let arrayStringTres:Array<string[]> = [["Hola","Mundo"]]; 
+    let arrayBoolean:boolean[] =[false,true,false];
+
+
+// Tupla
+     
+    let tupla:[number,string,boolean] = [1,"12",true];
